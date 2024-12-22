b9b47300947ec0babdf2831cba73a69aee29f2b4
Beimer Campos <beimer.developer@gmail.com>
2024-12-22 13:21:59 -0500

    feat(@dbeimer): initial structure react and tailwind

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,14 @@
+const Welcome = () => {
+  return (
+    <div className="m-4">
+      <h1 className="text-center text-xl">Welcome to Forjandi website</h1>
+      <p className="border rounded-md p-4 mt-4 to-slate-600">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, omnis?
+        Voluptatum nihil harum, error sed optio ipsum. Placeat odit nam quia.
+        Adipisci, esse. Quis reiciendis eligendi quod aut vel libero?
+      </p>
+    </div>
+  );
+};
+
+export default Welcome;
