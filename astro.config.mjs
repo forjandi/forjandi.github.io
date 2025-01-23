4dcf94718b9f3ab6c1016f1e51dde52ea89e70b9
Beimer Campos <beimer.developer@gmail.com>
2025-01-22 22:26:00 -0500

    feat(web-no card-@dbeimer): add theme mode button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
 });
+
