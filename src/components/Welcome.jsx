62c83af3b2c2d397b3911109211dd9c1b8571c3a
Beimer Campos <beimer.developer@gmail.com>
2025-01-22 23:12:12 -0500

    feat(web-no card-@dbeimer): first message and title

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,11 +1,15 @@
 const Welcome = () => {
   return (
     <div className="m-4">
-      <h1 className="text-center text-xl">Welcome to Forjandi website</h1>
-      <p className="border rounded-md p-4 mt-4 to-slate-600">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, omnis?
-        Voluptatum nihil harum, error sed optio ipsum. Placeat odit nam quia.
-        Adipisci, esse. Quis reiciendis eligendi quod aut vel libero?
+      <h1 className="text-center text-xl">Bienvenido a nuestro sitio web</h1>
+      <p className="border rounded-md p-4 mt-4">
+        Estamos trabajando arduamente en la construcción de nuestro nuevo sitio
+        web. Mientras tanto, si tienes alguna pregunta o necesitas contactarnos,
+        por favor envíanos un correo electrónico a{" "}
+        <a href="mailto:contacto@forjandi.com" className="text-blue-500">
+          contacto@forjandi.com
+        </a>
+        .
       </p>
     </div>
   );
