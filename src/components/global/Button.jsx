71169ae4d04381cc8f9f1e35531ec6a8605c0699
esmithas <esmith_as@hotmail.com>
2025-02-02 10:56:58 -0500

    refactor(about): Update properties Button component

--- src/components/global/Button.jsx
+++ src/components/global/Button.jsx
@@ -1,8 +1,8 @@
-const Button = ({ children, onClick, className = '', ...props }) => {
+const Button = ({ children, onClick, classNames = '', ...props }) => {
   return (
     <button
       onClick={onClick}
-      className={`font-inter-medium text-sm px-4 py-2 rounded-full transition-colors duration-300 bg-primary text-primary-foreground hover:bg-primary-dark dark:bg-primary-dark dark:hover:bg-primary ${className}`}
+      className={`font-inter-medium text-sm px-4 py-2 rounded-full transition-colors duration-300 bg-primary text-primary-foreground hover:bg-primary-dark dark:bg-primary-dark dark:hover:bg-primary ${classNames}`}
       {...props}
     >
       {children}
