ba127fdba0c63048638ede18b7bb2c592d8b92da
Beimer Campos <beimer.developer@gmail.com>
2025-02-05 21:24:44 -0500

    feat(web-no card-@dbeimer): add suggested changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ import Button from "./global/Button";
 const Hero = () => {
   return (
     <div className="container text-center max-w-screen-xl px-[20px] md:px-[80px] lg:px-[120px]">
-      <h1 className="text-[30px] leading-[40px] py-20 font-light md:text-[60-px] md:leading-[60px] lg:text-[80px] lg:leading-[100px]">
-        INTELIGENCIA ARTIFICIAL Y SOLUCIONES DE SOFTWARE A MEDIDA
+      <h1 className="text-[30px] leading-[40px] py-20 font-light md:text-[60-px] md:leading-[60px] lg:text-[80px] lg:leading-[100px] uppercase">
+        Inteligencia Artificial y Soluciones de Software a Medida
       </h1>
       <Button onClick={() => {}}>Contáctanos</Button>
     </div>
